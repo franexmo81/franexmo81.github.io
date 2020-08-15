fbb502e99ca8afc0647d80976213ac80217b37d7
Francisco Extremera <franexmo81@hotmail.com>
2020-08-15 09:13:34 +0200

    initial upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+let navBar = document.querySelector(".nav-bar");
+let hamburgerButton = document.querySelector(".nav-menu");
+
+hamburgerButton.addEventListener("click", handleButtonClick);
+
+function handleButtonClick (){
+  console.log("handleButtonClick");
+  hamburgerButton.classList.toggle("nav-menu--active");
+  navBar.classList.toggle("nav-bar--visible");
+  navBar.addEventListener("click", handleNavBarClick);
+}
+
+function handleNavBarClick (){
+  console.log("handleNavBarClick");
+  hamburgerButton.classList.remove("nav-menu--active");
+  navBar.classList.remove("nav-bar--visible");
+  document.removeEventListener("click", handleNavBarClick);
+}
